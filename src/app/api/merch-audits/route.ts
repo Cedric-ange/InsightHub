4d6266cf5a1ce5fcc6ff588487be0b76a5881717
speedo-os <crm.kimberlitegroupe@gmail.com>
2026-07-09 13:58:11 +0000

    fix: alignement de toutes les routes d'audits sur la connexion pooler fonctionnelle

--- src/app/api/merch-audits/route.ts
+++ src/app/api/merch-audits/route.ts
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
-    const sql = postgres(process.env.DATABASE_URL!, { ssl: "require" });
+    const sql = postgres("postgresql://postgres.jiksjctyvivyvmscryrt:AngeToure1234@aws-0-eu-west-1.pooler.supabase.com:6543/postgres?sslmode=require");
     const data = await sql`SELECT * FROM merch_audits ORDER BY created_at DESC`;
 
     const camelCaseData = data.map((m) => ({
@@ -17,8 +17,8 @@ export async function GET() {
     }));
 
     return NextResponse.json({ success: true, data: camelCaseData });
-    } catch (error: unknown) {
-    console.error("Erreur API:", error); // <-- Comme ça, 'error' est utilisé !
-    return NextResponse.json({ success: false, error: "Erreur serveur" }, { status: 500 });
-    }
+  } catch (error: unknown) {
+    console.error("Erreur API Login:", error);
+    return NextResponse.json({ success: false, error: "Erreur interne" }, { status: 500 });
+  }
 }
